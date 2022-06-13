8a0bdb7876365f405a870f468b16801b7e3f4e62
Carlos Nunez <karlosn1999@gmail.com>
2022-06-13 14:31:30 +0200

    fixed accuracy

--- R/metrics.R
+++ R/metrics.R
@@ -1,14 +1,7 @@
-# comprobar si d poblacional esta dentro del intervalo
-accuracy <- function(results, sample_size, d) {
+accuracy <- function(results, d) {
 
-  sd_d <- sqrt(
-    ((sample_size + sample_size) / sample_size^2) +
-      (d^2 / (2 * (sample_size + sample_size)))
-  )
-
-  #obtener intervalos de funciones del paquete
-  inside_interval <- length(which(results >= (d - 1.96 * sd_d) &
-                                    results <= (d + 1.96 * sd_d)))
+  inside_interval <- length(which(d >= results[[2]] &
+                                    d <= results[[3]]))
   outside_interval <- length(results) - inside_interval
 
   list(inside_interval = inside_interval,
@@ -17,14 +10,6 @@ accuracy <- function(results, sample_size, d) {
 
 
 empirical_power <- function(results) {
-
-  # sd <- sqrt(
-  #   ((sample_size + sample_size) / sample_size^2) +
-  #     (results^2 / (2 * (sample_size + sample_size)))
-  # )
-
-  #obtener intervalos de funciones del paquete
-  #lower_bound <- results - 1.96 * sd
 
   list(1-(length(which(results[[2]] < 0))/length(results)))
 }
