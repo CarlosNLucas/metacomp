30c230722cee907959a2fd732a49f51ebd9c26e5
Carlos Nunez <karlosn1999@gmail.com>
2022-06-16 15:13:55 +0200

    basic plotting function

---
+++ R/plotting.R
@@ -0,0 +1,9 @@
+
+plot <- function(matrizComb, results, parameter){
+  #check parameter is one inside matrizComb
+  matrizComb$x <- unlist(results)
+
+  ggplot2::ggplot(data=matrizComb, aes(x={{parameter}}, y=x))
+    + geom_point()
+    + geom_line()
+}
