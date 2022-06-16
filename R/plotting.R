0b94e520d5a8bd6287bf1b89169daf2ec3f7a0f4
Carlos Nunez <karlosn1999@gmail.com>
2022-06-16 15:31:40 +0200

    fix geo functs in new lines

--- R/plotting.R
+++ R/plotting.R
@@ -3,7 +3,7 @@ plot <- function(matrizComb, results, parameter){
   #check parameter is one inside matrizComb
   matrizComb$x <- unlist(results)
 
-  ggplot2::ggplot(data=matrizComb, aes(x={{parameter}}, y=x))
-    + geom_point()
-    + geom_line()
+  ggplot2::ggplot(data=matrizComb, aes(x={{parameter}}, y=x)) +
+    geom_point() +
+    geom_line()
 }
