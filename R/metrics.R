f7f8b79f51e744077523359b184b86ae0f880bf2
Carlos Nunez <karlosn1999@gmail.com>
2022-06-18 14:52:41 +0200

    docs for rejection_rate & empirical_power return change for rejection_rate & empirical_power, list removed

--- R/metrics.R
+++ R/metrics.R
@@ -1,3 +1,4 @@
+# TODO: docs
 accuracy <- function(results, d) {
 
   inside_interval <- length(which(d >= results[[2]] &
@@ -9,13 +10,38 @@ accuracy <- function(results, d) {
 }
 
 
+#' Calculation of empirical power, also known as acceptance rate,
+#' for a list of estimated effects
+#'
+#'
+#' @param results List of estimated effects in format
+#'  [ effect, lower bound, upper bound ]
+#'
+#' @return Empirical power, in range 0-1
+#' @export
+#'
+#' @examples
+#' rejection_rate(list(c(2,1,3), c(3,-1, 4), c(2, 1,4)))
+#' rejection_rate(estimated_effects)
 empirical_power <- function(results) {
 
-  list(1-(length(which(results[[2]] < 0))/length(results)))
+  1-(length(which(results[[2]] < 0))/length(results))
 }
 
 
+#' Calculation of rejection rate for a list of estimated effects
+#'
+#'
+#' @param results List of estimated effects in format
+#'  [ effect, lower bound, upper bound ]
+#'
+#' @return Rate of rejection, in range 0-1
+#' @export
+#'
+#' @examples
+#' rejection_rate(list(c(2,1,3), c(3,-1, 4), c(2, 1,4)))
+#' rejection_rate(estimated_effects)
 rejection_rate <- function(results) {
 
-  list(1-(length(which(results[[2]] > 0))/length(results)))
+  1-(length(which(results[[2]] > 0))/length(results))
 }
