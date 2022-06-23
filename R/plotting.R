d4a5408bd9b838516b14c713ef8a65c403b686a5
Carlos Nunez <karlosn1999@gmail.com>
2022-06-23 11:37:33 +0200

    plot now simulates, sends extra parameters to AES() & doc half ass'd

--- R/plotting.R
+++ R/plotting.R
@@ -1,9 +1,28 @@
 
-plot <- function(matrizComb, results, parameter){
-  #check parameter is one inside matrizComb
-  matrizComb$x <- unlist(results)
+#' Plot a simple simulation with a single parameter
+#'
+#' @param matrizComb
+#' @param method
+#' @param metric
+#' @param parameter
+#'
+#' @return a plot ?
+#' @export
+#'
+#' @examples
+plot <- function(matrizComb, method, metric, parameter, ...) {
+  # TODO: check parameter is one inside matrizComb
 
-  ggplot2::ggplot(data=matrizComb, aes(x={{parameter}}, y=x)) +
+  results <- montecarlo(matrizComb, method, metric)
+  matrizComb$results <- unlist(results)
+
+  ggplot2::ggplot(data=matrizComb, aes(x={{parameter}}, y=results, ...)) +
     geom_point() +
     geom_line()
+}
+
+plot_compare_methods <- function(matrizComb, results_1, results_2, parameter) {
+  # ggplot(matrizComb, aes(x=n_experiments))+
+  #   geom_line(aes(y=x, colour = "hedges")) +
+  #   geom_line(aes(y=y, colour = "PRR"))
 }
