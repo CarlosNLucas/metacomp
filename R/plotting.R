18a5aff01332854a8ad8a3eac929042903eb40b6
Carlos Núñez <47300544+CarlosNLucas@users.noreply.github.com>
2022-11-05 18:36:37 +0100

    Update plotting.R

--- R/plotting.R
+++ R/plotting.R
@@ -1,15 +1,3 @@
-
-#' Plot a simple simulation with a single parameter
-#'
-#' @param matrizComb
-#' @param method
-#' @param metric
-#' @param parameter
-#'
-#' @return a plot ?
-#' @export
-#'
-#' @examples
 plot_sim <- function(matrizComb, method, metric, parameter, ...) {
   # TODO: check parameter is one inside matrizComb
 
@@ -29,20 +17,33 @@ plot <- function(matrizComb, results, parameter, ...){
     ggplot2::geom_line()
 }
 
+#' Plot
+#'
+#' @param matrizComb
+#' @param method
+#' @param parameter
+#'
+#' @return
+#' @export
+#'
+#' @examples
 plot_compare_methods <- function(matrizComb,
-                                 method_1,
-                                 method_2,
                                  metric,
                                  parameter,
                                  ...) {
 
-  results_1 <- montecarlo(matrizComb, method_1, metric)
-  results_2 <- montecarlo(matrizComb, method_2, metric)
+  require(ggplot2)
 
-  matrizComb$results_1 <- unlist(results_1)
-  matrizComb$results_2 <- unlist(results_2)
+  metric_name  <- as.character(substitute(metric))
+  metric_name <- paste(metric_name[2], "::", metric_name[3], sep="")
 
-  ggplot(matrizComb, aes(x={{parameter}}))+
-    geom_line(aes(y=results_1, colour = "{{method_1}}", ...)) +
-    geom_line(aes(y=results_2, colour = "{{method_2}}", ...))
+  parameter <- sym(parameter)
+  metric_name <- sym(metric_name)
+
+  ggplot(matrizComb, aes(x = !!parameter,
+                         y = !!metric_name,
+                         group = methods,
+                         colour = methods)) +
+  stat_summary(geom = "line",
+               fun = mean)
 }
